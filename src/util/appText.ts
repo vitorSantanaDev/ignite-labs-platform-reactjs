901a1dd1aec610892c9b140e083a436c41dba87e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-24 18:31:25 -0700

    chore: :seedling: putting all the static texts of the application in a single file

---
+++ src/util/appText.ts
@@ -0,0 +1,17 @@
+export const APP_TEXT = {
+  ASIDE_TITLE: "Cronograma das aulas",
+  LIVE_TEXT: "Ao vivo",
+  PRATICAL_CLASS: "Aula prática",
+  LESSONCARD_TEXT_CONTENT_RELEASED: "Conteúdo liberado",
+  LESSONCARD_TEXT_COMING_SOON: "Em breve",
+  BUTTON_TEXT_COMMUNITY_OF_DISCORD: "Comunidade do discord",
+  CHALLENGE_ACCESS_TEXT_BUTTON: "Acesse o desafio",
+  SUPPLEMENTARY_MATERIAL_CARD_TITLE: "Material complementar",
+  SUPPLEMENTARY_MATERIAL_CARD_DESCRIPTION:
+    "Acesse o material complementar para acelerar o seu desenvolvimento",
+  EXCLUSIVE_WALLPAPERS_CARD_TITLE: "Wallpapers exclusivos",
+  EXCLUSIVE_WALLPAPERS_CARD_DESCRIPTION:
+    "Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina",
+  FOOTER_CONTENT: "Rocketseat	- Todos os direitos reservados",
+  LINK_TEXT_FOOTER: "Políticas de privacidade",
+};
