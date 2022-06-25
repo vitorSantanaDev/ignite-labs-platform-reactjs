1aa66eefd47bfadf4ba8385ff12c849d11a85245
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-24 18:29:21 -0700

    chore: :seedling: exporting all components from a single file

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Logo } from "./Logo";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as LessonCard } from "./LessonCard";
+export { default as Sidebar } from "./Sidebar";
+export { default as VideoPlayer } from "./VideoPlayer";
+export { default as LogoRocketseat } from "./LogoRocketseat";
