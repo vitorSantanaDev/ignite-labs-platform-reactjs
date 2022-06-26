96b6f1d793503f1601fb99f8040c6ccb1c8df050
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-25 17:58:26 -0700

    feat: :sparkles: adding new styles to the component

--- src/components/LessonCard/index.tsx
+++ src/components/LessonCard/index.tsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { CheckCircle } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import classNames from "classnames";
 
 import { APP_TEXT } from "../../util/appText";
 
@@ -9,6 +10,7 @@ import { ClassroomTypeEnum, ILessonCardProps } from "./types";
 
 export default function LessonCard(props: ILessonCardProps) {
   const { title, lessonType, availableAt, slug } = props;
+  const { slug: slugRoute } = useParams<{ slug: string }>();
 
   const isLessonAvailable = isPast(availableAt);
 
@@ -20,13 +22,28 @@ export default function LessonCard(props: ILessonCardProps) {
     }
   );
 
+  const isActiveLesson = slugRoute === slug;
+
   return (
     <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div
+        className={classNames("rounded border border-gray-500 p-4 mt-2", {
+          "bg-green-500 text-gray-50": isActiveLesson,
+          "group-hover:border-green-500": !isActiveLesson,
+        })}
+      >
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span
+              className={classNames(
+                "text-sm font-medium flex items-center gap-2",
+                {
+                  "text-gray-50": isActiveLesson,
+                  "text-blue-500": !isActiveLesson,
+                }
+              )}
+            >
               <CheckCircle size={20} />
               {APP_TEXT.LESSONCARD_TEXT_CONTENT_RELEASED}
             </span>
@@ -36,13 +53,28 @@ export default function LessonCard(props: ILessonCardProps) {
               {APP_TEXT.LESSONCARD_TEXT_COMING_SOON}
             </span>
           )}
-          <span className="text-xs uppercase rounded py-[0.125rem] px-2 text-whie border border-green-300 font-bold">
+          <span
+            className={classNames(
+              "text-xs uppercase rounded py-[0.125rem] px-2 text-whie border font-bold",
+              {
+                "border-green-300": !isActiveLesson,
+                "border-gray-50": isActiveLesson,
+              }
+            )}
+          >
             {lessonType === ClassroomTypeEnum.CLASS
               ? APP_TEXT.PRATICAL_CLASS
               : APP_TEXT.LIVE_TEXT}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">{title}</strong>
+        <strong
+          className={classNames("mt-5 block", {
+            "text-gray-50": isActiveLesson,
+            "text-gray-200": !isActiveLesson,
+          })}
+        >
+          {title}
+        </strong>
       </div>
     </Link>
   );
